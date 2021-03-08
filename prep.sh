24d176acf1243b894ea89e45dc69a7cb519d09c3
bitlox <dana.coe@bitlox.com>
2021-03-08 11:51:09 +0000

    Separate out the setup for the data

--- prep.sh
+++ prep.sh
@@ -31,33 +31,33 @@ mkdir data
 
 pwd > data/pwd.txt
 
-function setCustomDepool {
-  echo
-  echo -e "\tDestination DePool address:"  
-  read inputDepool
-  customDepool=$inputDepool
-  echo $customDepool > data/destination.addr
-}
+# function setCustomDepool {
+#   echo
+#   echo -e "\tDestination DePool address:"  
+#   read inputDepool
+#   customDepool=$inputDepool
+#   echo $customDepool > data/destination.addr
+# }
+# 
+# function setCustomValidator {
+#   echo
+#   echo -e "\tValidator wallet address:"  
+#   read inputValidator
+#   customValidator=$inputValidator
+#   echo $inputValidator > data/validator.addr
+# }
+# 
+# function setCustomPhrase {
+#   echo
+#   echo -e "\tSecret phrase for the signing (no quotes):"  
+#   read inputPhrase
+#   customPhrase=$inputPhrase
+#   echo $inputPhrase > data/phrase.txt
+# }
+# 
+# setCustomDepool
+# setCustomValidator
+# setCustomPhrase
 
-function setCustomValidator {
-  echo
-  echo -e "\tValidator wallet address:"  
-  read inputValidator
-  customValidator=$inputValidator
-  echo $inputValidator > data/validator.addr
-}
-
-function setCustomPhrase {
-  echo
-  echo -e "\tSecret phrase for the signing (no quotes):"  
-  read inputPhrase
-  customPhrase=$inputPhrase
-  echo $inputPhrase > data/phrase.txt
-}
-
-setCustomDepool
-setCustomValidator
-setCustomPhrase
-
-echo "READY"
+echo "Nest step: Please run ./setup.sh"
 exit
