ccec0458979276e85fdf3db6fb09187bfef71788
bitlox <dana.coe@bitlox.com>
2021-03-08 12:11:10 +0000

    delete intermediates

--- signer.sh
+++ signer.sh
@@ -1,6 +1,6 @@
 working_dir=$(cat data/pwd.txt)
 tx_id=$(cat result3.txt)
 ${working_dir}/./tonos-cli --url https://main.ton.dev call `cat ${working_dir}/data/validator.addr`  confirmTransaction '{"transactionId": '${tx_id}'}' --abi ${working_dir}/SafeMultisigWallet.abi.json  --sign "`cat ${working_dir}/data/phrase.txt`" 
-# rm result1.txt
-# rm result2.txt
-# rm result3.txt
+rm result1.txt
+rm result2.txt
+rm result3.txt
