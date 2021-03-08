5632339bd501a4c5f4567a0dcde4a6d30b7fbbe7
bitlox <dana.coe@bitlox.com>
2021-03-08 11:59:41 +0000

    Edit remove commented code

--- prep.sh
+++ prep.sh
@@ -32,33 +32,5 @@ mkdir data
 
 pwd > data/pwd.txt
 
-# function setCustomDepool {
-#   echo
-#   echo -e "\tDestination DePool address:"  
-#   read inputDepool
-#   customDepool=$inputDepool
-#   echo $customDepool > data/destination.addr
-# }
-# 
-# function setCustomValidator {
-#   echo
-#   echo -e "\tValidator wallet address:"  
-#   read inputValidator
-#   customValidator=$inputValidator
-#   echo $inputValidator > data/validator.addr
-# }
-# 
-# function setCustomPhrase {
-#   echo
-#   echo -e "\tSecret phrase for the signing (no quotes):"  
-#   read inputPhrase
-#   customPhrase=$inputPhrase
-#   echo $inputPhrase > data/phrase.txt
-# }
-# 
-# setCustomDepool
-# setCustomValidator
-# setCustomPhrase
-
 echo "Next step: Please run ./setup.sh"
 exit
